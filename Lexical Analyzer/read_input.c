2010206a86d5516313bd28fd35dcaed803896c6c
Yash Pandey <ypandey474@gmail.com>
2024-01-20 23:26:47 +0530

    Co-authored-by: achinthyahebbar@gmail.com <achinthyahebbar@gmail.com> Co-authored-by: Hardik Gupta <typhoons01@users.noreply.github.com> Co-authored-by: Vansh0409 <Vansh0409@users.noreply.github.com>

--- Lexical Analyzer/read_input.c
+++ Lexical Analyzer/read_input.c
@@ -1,9 +1,78 @@
 //SCHEME: TWO-INPUT BUFFERS EACH OF SIZE N WITH SENTINEL CHARACTER "EOF" MARKING THE END OF EACH BUFFER
 
+//INCLUDE LIBRARIES
+#include <stdio.h>
+
 //PARAMETER N = NUMBER OF CHARACTERS READ WITH EACH RELOAD
+#define BUFFER_SIZE 256
+//TYPICALLY 256 BYTES
 
 // EOF CHARACTERS AT END OF BOTH BUFFERS ALWAYS
 
 // FUNCTIONS TO READ "RELOAD" INTO BUFFER FROM A FILE 
 
 // CODE TO READ FROM THE BUFFERS CHARACTER BY CHARACTER
+struct TwinBuffer{
+    int readingFirst;
+    char buffer[BUFFER_SIZE*2 + 2];
+};
+
+FILE* readTestFile(char* file_path){
+    FILE *file = fopen(file_path, "r");
+
+    //UNABLE TO READ FILE
+    if (file == NULL){
+        printf("Error opening file %s\n", file_path);
+        return NULL;
+    }
+
+    return file;
+
+}
+void readIntoBuffer(struct TwinBuffer* twinBuffer, FILE* file){
+    char* buffer;
+    if (twinBuffer->readingFirst)
+    {
+        buffer=twinBuffer->buffer;
+    }
+    else
+    {
+        buffer=twinBuffer->buffer+BUFFER_SIZE+1;
+    }
+
+    twinBuffer->readingFirst = 1-twinBuffer->readingFirst;
+    size_t read_bytes = fread(buffer, sizeof(char), BUFFER_SIZE, file);
+}
+
+//CREATE THE BUFFER AND RETURN IT
+struct TwinBuffer initialiseTwinBuffer(){
+    //INITIALISE BUFFER
+    struct TwinBuffer twinBuffer;
+
+    twinBuffer.readingFirst = 1;
+    twinBuffer.buffer[BUFFER_SIZE] = '\0';
+    twinBuffer.buffer[2*BUFFER_SIZE + 1] = '\0';
+
+    return twinBuffer;
+}
+
+
+int main(){
+    //TEST THE TWIN BUFFER
+    FILE* file = readTestFile("test_program.txt");
+
+    //INITIALISE A TWIN BUFFER
+    struct TwinBuffer twinBuffer = initialiseTwinBuffer();
+
+    //READ INTO THE BUFFER
+    readIntoBuffer(&twinBuffer, file);
+    readIntoBuffer(&twinBuffer, file);
+
+    //CHECK
+    for (int i = 0; i<BUFFER_SIZE*2 + 2; i += 1){
+        printf("%c", twinBuffer.buffer[i]);
+    }
+    
+}
+
+
--- Lexical Analyzer/symbol_table.c
+++ Lexical Analyzer/symbol_table.c
@@ -1,7 +1,35 @@
 // MAKE A HASH TABLE AS THE SYMBOL TABL
+#define hashMapSize 69;
+struct SymbolTable{
+    
+}
+//EACH ENTRY IN THE SYMBOL TABLE: HASH ON THE LEXEME
+struct symbolTableEntry{
+    char* lexeme;
+    enum Tokentype token;
+};
 
+int hash(const char *lexeme) {
+    unsigned int hash = 0;
+    while (*lexeme) {
+        hash = hash * 31 + *(lexeme++);
+    }
+    return hash % hashMapSize;
+}    
+
+int lookup(struct SymbolTable* symboltableonst char *lexeme) {
+    int index = hash(key);
+    while (hashMap[index].key != NULL) {
+        if (strcmp(hashMap[index].key, key) == 0) {
+            return hashMap[index].value;
+        }
+        index = (index + 1) % hashMapSize;
+    }
+    return -1;
+}
 // STORE LEXEMES IN A SEPARATE DATA STRUCTURE (?)
 
 // ADD THE KEYWORDS <TOKEN, POINTER TO LEXEME>
 
 // WRITE FUNCTIONS FOR INSERT (TOKEN, LEXEME) AND LOOKUP (LEXEME)
+
